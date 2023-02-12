3dd6ae109d0e7a37c20a33b08c4740489ecd485e
Borreguito <mmmunapregunta@gmail.com>
2023-02-11 19:08:00 -0600

    Agregadas algunas cosas

---
+++ Actividad2/src/main/java/com/unam/actividad2/Actividad2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.unam.actividad2;
+
+/**
+ *
+ * @author angel
+ */
+public class Actividad2 {
+
+    public static void main(String[] args) {
+        libro1 = 
+    }
+}
---
+++ Actividad2/src/main/java/com/unam/actividad2/Libro.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.unam.actividad2;
+
+/**
+ *
+ * @author angel
+ */
+public class Libro {
+
+    private int id;
+    private String nombre;
+    private String editorial;
+    
+    
+    
+    public Libro(int id, String nombre, String editorial) {
+        this.id = id;
+        this.nombre = nombre;
+        this.editorial = editorial;
+    }
+}
